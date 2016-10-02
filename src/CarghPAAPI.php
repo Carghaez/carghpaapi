4893f8e7aa34d78d4926abe5d161cb261054ad2b
Carghaez <kejan73@gmail.com>
2016-10-02 14:27:14 +0200

    aggiunto getCategories

--- src/CarghPAAPI.php
+++ src/CarghPAAPI.php
@@ -56,6 +56,97 @@ class CarghPAAPI
         return $this->applyResponseTransformer($response);
     }
 
+    public static function getCategories($country)
+    {
+        $categories = [
+            'All',
+            'Apparel',
+            'Grocery',
+            'MobileApps',
+            'Automotive',
+            'Beauty',
+            'GiftCards',
+            'Music',
+            'OfficeProducts',
+            'Kitchen',
+            'HealthPersonalCare',
+            'Electronics',
+            'Tools',
+            'DVD',
+            'Garden',
+            'Toys',
+            'Jewelry',
+            'Lighting',
+            'Industrial',
+            'PCHardware',
+            'KindleStore',
+            'Books',
+            'ForeignBooks',
+            'MP3Downloads',
+            'Watches',
+            'Baby',
+            'Shoes',
+            'Software',
+            'SportingGoods',
+            'MusicalInstruments',
+            'Luggage',
+            'VideoGames'
+        ];
+
+        $categorie = [
+            'Tutte le categorie',
+            'Abbigliamento',
+            'Alimentari e cura della casa',
+            'App e Giochi',
+            'Auto e Moto',
+            'Bellezza',
+            'Buoni Regalo',
+            'CD e Vinili',
+            'Cancelleria e prodotti per ufficio',
+            'Casa e cucina',
+            'Cura della Persona',
+            'Elettronica',
+            'Fai da te',
+            'Film e TV',
+            'Giardino e giardinaggio',
+            'Giochi e giocattoli',
+            'Gioielli',
+            'Illuminazione',
+            'Industria e Scienza',
+            'Informatica',
+            'Kindle Store',
+            'Libri',
+            'Libri in altre lingue',
+            'Musica Digitale',
+            'Orologi',
+            'Prima infanzia',
+            'Scarpe e borse',
+            'Software',
+            'Sport e tempo libero',
+            'Strumenti musicali e DJ',
+            'Valigeria',
+            'Videogiochi'
+        ];
+
+        switch ($country) {
+            case 'it':
+                $terms = $categorie;
+                break;
+            default:
+                $terms = $categories;
+                break;
+        }
+        if (count($terms) !== count($categories)) {
+            $terms = $categories;
+        }
+
+        $results = [];
+        foreach ($categories as $key => $value) {
+            $results[$value] = $terms[$key];
+        }
+        return $results;
+    }
+
     /**
      * Applies a responsetransformer.
      *
